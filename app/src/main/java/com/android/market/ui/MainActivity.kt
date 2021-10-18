57ffb0363cfdaf5550e35d28126d838047c1fedd
serkan-ozdemir <56133743+serkan-ozdemir@users.noreply.github.com>
2021-10-18 21:02:34 +0300

    Navigation added

---
+++ app/src/main/java/com/android/market/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.android.market.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.market.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
